4262b25bae69513d2dd58178b1eec28cc3754942
Christopher Comella <git.chrisco@gmail.com>
2016-05-31 22:02:48 -0600

    Wire-up 'add film' page. JS. Style. Edit README.

--- README.md
+++ README.md
@@ -4,3 +4,29 @@
 
 See it on the web at:    
 https://gfilms.firebaseapp.com
+
+## Wireframes
+
+![Wireframes](wireframes/wireframes.jpg)
+
+![Mockup](wireframes/HomePage1.png)
+
+## API (via Kyle)
+
+Endpoint:
+https://mighty-eyrie-15280.herokuapp.com/films
+
+if you make a POST request to that endpoint with an object that looks like:
+
+ ```{
+    title: "Ghost",
+    genre: "Good",
+    description: "Uh",
+    coverPicture: "https://www.google.com",
+    rating: "R"
+}
+```
+
+You'll get a positive response.
+
+Everything else will get a `400 Bad Request`
--- public/app.js
+++ public/app.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-  console.log("Foo");
+	$(".add-film-form").submit(function(event) {
+		event.preventDefault()
+		var data = $(this).serialize();
+		// $.post('https://mighty-eyrie-15280.herokuapp.com/films', data, function(data, textStatus, xhr) {
+		// 	console.log(data);
+		// 	/*optional stuff to do after success */
+		// 	$("#results").append(data.message);
+		// });
+		$.ajax({
+			url: 'https://mighty-eyrie-15280.herokuapp.com/films',
+			type: 'POST',
+			dataType: 'json',
+			data: data
+		})
+		.done(function(data) {
+			console.log("success");
+			$("#results").append(data.message);
+		})
+		.fail(function(error) {
+			var msg = JSON.parse(error.responseText).message;
+			$("#results").append(msg);
+		})
+		.always(function() {
+			console.log("complete");
+		});
+	});
 });
